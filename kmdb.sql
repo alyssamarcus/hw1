5f090aac1cf3230eea0f1ae5a88fcab3563cadb9
alyssamarcus <alyssa.marcus@kellogg.northwestern.edu>
2022-04-21 23:45:59 +0000

    edits

--- kmdb.sql
+++ kmdb.sql
@@ -112,50 +112,76 @@ CREATE TABLE movies(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     title TEXT,
     year TEXT,
-    MPAA rating TEXT,
-    studio_id TEXT,
-    actor_id TEXT,
-    character_id TEXT,
+    MPAA_rating TEXT,
+    studio_id TEXT
 );
 
 CREATE TABLE studios(
     id INTEGER PRIMARY KEY AUTOINCREMENT, 
-    studio TEXT,
-    movie_id INTEGER,
+    studio_name TEXT,
+    movie_id INTEGER
 );
 
 CREATE TABLE actors(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    actor_name TEXT,
-    movie_id INTEGER, 
-    character_id INTEGER,
+    actor_name TEXT
 );
 
 CREATE TABLE characters(
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     character_name TEXT,
     movie_id INTEGER, 
-    actor_id INTEGER,
+    actor_id INTEGER
 );
 
 -- Insert data into your database that reflects the sample data shown above
 -- Use hard-coded foreign key IDs when necessary
 
+INSERT INTO studios (studio_name)
+VALUES("Warner Bros.");
 
+INSERT INTO movies (title, year, MPAA_rating, studio_id)
+VALUES ("Batman Begins", "2005", "PG-13", 1),
+("The Dark Knight", "2008", "PG-13", 1),
+("The Dark Knight Rises", "2012", "PG-13", 1);
+
+INSERT INTO actors (actor_name)
+VALUES ("Christian Bale"), ("Michael Caine"), ("Liam Neeson"), ("Katie Holmes"), ("Gary Oldman"), 
+("Heath Ledger"), ("Aaron Eckhart"), ("Maggie Gyllenhaal"), ("Tom Hardy"), ("Joseph Gordon-Levitt"), ("Anne Hathaway");
+ 
+ INSERT INTO characters (character_name, movie_id, actor_id)
+ values ("Bruce Wayne", 1, 1),("Afred",1, 2), ("Ra's Al Ghul", 1, 3), ("Rachel Dawes", 1,4), ("Commissioner Gordon", 1, 5),
+ ("Bruce Wayne", 2,1), ("Joker", 1,6), ("Harvey Dent", 2, 7), ("Alfred", 2, 2), ("Rachel Dawes", 2, 8), ("Bruce Wayne", 3, 1), 
+ ("Commissioner Gordon", 3,5), ("Bane", 3, 9), ("John Blake", 3, 10), ("Selina Kyle", 3, 11);
 
 -- Prints a header for the movies output
 
+SELECT * FROM studios;
+
+.width 30 30 30
+.print "Movies"
+.print "====="
+.print ""
 
 -- The SQL statement for the movies output
 
-
-
+SELECT movies.title, movies.year, movies.MPAA_rating, studios.studio_name
+FROM movies 
+INNER JOIN studios 
+ON movies.studio_id = studios.id;
 
 -- Prints a header for the cast output
 
-
+.print ""
+.print "Top Cast"
+.print "======="
+.print ""
+SELECT * FROM actors;
+SELECT * FROM characters;
 
 -- The SQL statement for the cast output
 
-
-
+SELECT movies.title, actors.actor_name, characters.character_name
+FROM characters
+INNER JOIN actors ON characters.actor.id = actors.id
+INNER JOIN movies ON characters.movie_id = movies.id;
